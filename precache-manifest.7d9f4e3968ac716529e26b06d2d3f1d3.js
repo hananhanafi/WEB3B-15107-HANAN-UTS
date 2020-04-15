b8e1e30c1578e169b2439f0ac582834ff1f654a9
hananhanafi <hananhanafi@mail.ugm.ac.id>
2020-04-15 15:32:54 +0700

    Updates

---
+++ precache-manifest.7d9f4e3968ac716529e26b06d2d3f1d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67fc8a40240089a957b492b66312dc1",
+    "url": "/hananhanafi/WEB3B-15107-HANAN-UTS/index.html"
+  },
+  {
+    "revision": "47cde5f6e5cd9d2bd5fd",
+    "url": "/hananhanafi/WEB3B-15107-HANAN-UTS/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "393fb81ea8f070d11131",
+    "url": "/hananhanafi/WEB3B-15107-HANAN-UTS/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "47cde5f6e5cd9d2bd5fd",
+    "url": "/hananhanafi/WEB3B-15107-HANAN-UTS/static/js/2.bf9115e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hananhanafi/WEB3B-15107-HANAN-UTS/static/js/2.bf9115e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "393fb81ea8f070d11131",
+    "url": "/hananhanafi/WEB3B-15107-HANAN-UTS/static/js/main.52450cf4.chunk.js"
+  },
+  {
+    "revision": "cb1596e94ec7578a2c91",
+    "url": "/hananhanafi/WEB3B-15107-HANAN-UTS/static/js/runtime-main.2cd56c07.js"
+  }
+]);
